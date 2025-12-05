0a2aa365d765d634dd82f1daf8981baaef19a09e
Huyenthuong <huyenthuongnguyen43@gmail.com>
2025-12-05 17:31:06 +0700

    adddd

---
+++ test.js
@@ -0,0 +1,8 @@
+// test noti
+import { apiGet, apiPost } from "./client";
+
+export const getNotifications = (unreadOnly = false) =>
+  apiGet(`/notifications${unreadOnly ? "?unread=1" : ""}`);
+
+export const markNotificationRead = (id) =>
+  apiPost(`/notifications/${id}/read`);
